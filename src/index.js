9b357c1edfe75076c2f80f6ec3db54d4ae84ba5d
Ivan Santos <pragmaticivan@gmail.com>
2017-05-04 14:12:09 -0700

    Adds response as the second param on getInitialState function

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ export default {
 
           let data;
           if (isFunction(getInitialState)) {
-            data = await getInitialState(req) || {};
+            data = await getInitialState(req, res) || {};
           }
 
           data.__MAGNET_PAGE__ = module.default.name;
