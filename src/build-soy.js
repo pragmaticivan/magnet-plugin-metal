f54586f141b748474385d4d665969f34a6c25469
Ivan Santos <pragmaticivan@gmail.com>
2017-11-13 14:12:54 -0800

    Adds option to specify Soy Dependencies on magnet metal config

--- src/build-soy.js
+++ src/build-soy.js
@@ -1,9 +1,9 @@
 import soy from 'metal-tools-soy';
 
-const buildSoyFiles = (src, dest) =>
+const buildSoyFiles = (src, dest, soyDeps) =>
   new Promise((resolve, reject) => {
     const handleError = error => reject(error);
-    soy({src, dest, handleError}).on('end', () => resolve());
+    soy({src, dest, soyDeps, handleError}).on('end', () => resolve());
   });
 
   export default async (magnet) => {
@@ -20,11 +20,16 @@ const buildSoyFiles = (src, dest) =>
       dest = metalConfig.soyDest;
     }
 
+    let soyDeps = ['node_modules/metal*/src/**/*.soy'];
+    if (metalConfig && metalConfig.soyDeps) {
+      soyDeps = metalConfig.soyDeps;
+    }
+
     // Trivially excludes soy compilation when there are no matching soy files
     // in the application directory.
     const directory = magnet.getDirectory();
     const isTriviallyExcluded = magnet.getFiles({directory, src}).length === 0;
     if (!isTriviallyExcluded) {
-      await buildSoyFiles(src, dest);
+      await buildSoyFiles(src, dest, soyDeps);
     }
   };
