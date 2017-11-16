e970d518d87c555016013b69ec6af475b024171a
Ivan Santos <pragmaticivan@gmail.com>
2017-11-15 17:28:09 -0800

    Updates packages and removes JSDOM

--- test/setup/node.js
+++ test/setup/node.js
@@ -1,14 +1,6 @@
 import chai from 'chai';
-import jsdom from 'jsdom';
-
-const {JSDOM} = jsdom;
-const dom = new JSDOM();
-global.document = dom.window.document;
-global.window = dom.window;
 
 process.env.NODE_ENV = 'test';
-
 global.chai = chai;
 
-require('babel-core/register');
 require('./setup')();
